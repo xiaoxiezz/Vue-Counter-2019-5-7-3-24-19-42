db2e7a1fe14c3af63a9738c10d4136d42a24a27a
xiexinfang <1036554353@qq.com>
2019-09-02 11:07:09 +0800

    counter

---
+++ counter/src/components/Counter.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="counter">
+    <p >{{number}}</p>
+    <button v-on:click="add">+</button>
+    <button v-on:click="reduce">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+        name: 'Counter',    
+        data(){
+            return {
+                number: 0
+            }
+        },
+ methods: {
+     add: function(){
+         this.number ++;
+     },
+     reduce: function(){
+         this.number --;
+     }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
